d653e0a93c000c0fd1fd2b09e1fde11695e79ff7
shanghanrun <idim7@naver.com>
2024-02-27 18:49:01 +0900

    기본설정

---
+++ js/main.js
@@ -0,0 +1,218 @@
+const newsData =[
+      {
+        'title':"'파묘' 유해진 '언덕 오르는 리어카 밀어주는 역할이라 생각'",
+        "sub_title":"장의사 역으로 최민식·김고은·이도현과 연기 앙상블 '근래 보기 드문 신선한 작품…참여한 것만으로도 의미'",
+        "content": `"제가 작품을 끌고 가는 역할을 할 수도 있지만, 어떻게 매번 그러겠어요. 이번에는 그냥 작품과 함께 걸어가는 동반자 입장이었던 것 같아요." <br>장재현 감독의 오컬트 영화 '파묘'에 출연한 배우 유해진은 26일 서울 종로구 한 카페에서 한 인터뷰에서 "참여하는 것만으로도 의미 있는 작품"이라며 이같이 말했다. <br>'파묘'는 거액의 돈을 받고 수상한 묘를 이장하게 된 사람들이 겪는 기이한 일을 그린 영화다. 유해진은 대통령의 장례를 맡을 정도로 유능한 장의사 영근 역을 맡았다. 풍수사 상덕(최민식 분), 무당 화림(김고은), 화림의 제자 봉길(이도현)과의 앙상블이 돋보인다. <br>유해진은 그동안 자기가 주인공인 작품을 주로 해왔지만, 이번에는 주연 4인방의 한 축을 담당한다.`,
+        "imgUrl":'https://img4.yna.co.kr/etc/inner/KR/2024/02/26/AKR20240226141500005_01_i_P4.jpg',
+        "createdAt":'2024-02-26',
+        "source": "https://www.yna.co.kr/view/AKR20240226141500005?section=entertainment/movies"
+      },
+      {
+        'title':`김고은 "포스 있는 무당 캐릭터, 어설프게 표현하긴 싫었죠"`,
+        'subtitle': `'파묘'서 무녀 역…"굿 보러 다니고 경문 통째 외우며 준비" <br>"관객 수 비현실적으로 다가와…꽉 찬 객석에 뭉클"`,
+        "content":`장재현 감독의 오컬트 영화 '파묘'는 예고편 공개 때부터 배우 김고은이 굿을 하는 장면으로 화제가 됐다.<br>그는 거액의 돈을 받고 수상한 묘를 이장하게 된 사람들이 겪는 기이한 일을 그린 이 영화에서 젊은 무녀 화림 역을 맡았다.<br>서슬 퍼런 식칼을 든 채 신들린 듯 춤을 추다가 숱을 얼굴에 바르는 대살 굿 장면은 '파묘'의 백미다.<br>"시나리오를 처음 봤을 때 굉장히 포스 있고 아우라 넘치는 캐릭터라고 생각했어요. 이런 역할을 어설프게 표현하고 싶진 않았습니다."`,
+        "imgUrl":`https://img3.yna.co.kr/photo/cms/2024/02/22/74/PCM20240222000074005_P4.jpg`,
+        "createdAt":`2024-02-26`,
+        "source": `https://www.yna.co.kr/view/AKR20240226125000005?section=entertainment/movies`
+      },
+      {
+        'title':`스티븐 연, 美배우조합상 남우주연상…TV부문 주요 4개상 석권`,
+        'subtitle': `그레타 리가 트로피 건네 '한국계 위상' 확인…故이선균, 추모 영상에 등장<br>'오펜하이머', 영화부문 최고상과 남우주·조연상 3관왕…'바비'는 무관`,
+        "content":`24일(현지시간) 미국 로스앤젤레스(LA)에서 열린 제30회 배우조합상(SAG) 시상식에서 스티븐 연은 TV영화·미니시리즈 부문 남우주연상 수상자로 호명됐다.<br>스티븐 연은 이번 수상으로 지난달 골든글로브 시상식과 에미상 시상식, 크리틱스초이스 시상식에 이어 올해 미국 주요 시상식 4개의 TV 미니시리즈 부문 남우주연상을 모두 석권하게 됐다.<br>특히 배우조합상은 할리우드의 동료 배우들이 연기력을 인정했다는 점에서 배우들에게는 남다른 의미를 지닌다.`,
+        "imgUrl":`https://img7.yna.co.kr/photo/etc/af/2024/02/25/PAF20240225283101009_P4.jpg`,
+        "createdAt":`2024-02-25`,
+        "source": `https://www.yna.co.kr/view/AKR20240225022751075?section=entertainment/movies`
+      },
+      {
+        'title':`마동석 펀치에 관객 주먹 불끈…'범죄도시4' 베를린서 첫선`,
+        'subtitle': `유머＋액션 기존 구성에 첨단범죄로 폭 넓혀<br>1∼3편 변주 또는 오마주…'신스틸러' 박지환 등장 때마다 폭소<br>"지금까지 시리즈 중 최고…할리우드에서 보기 힘든 액션 영화"`,
+        "content":`24일 0시5분(현지시간) 독일 베를린영화제 메인 상영관인 베를리날레 팔라스트. '범죄도시' 시리즈 특유의 경쾌한 음악과 함께 엔딩 크레디트가 올라가자 2층까지 2천여 석을 가득 메운 관객들 사이에서 휘파람과 함께 환호성이 터져나왔다.<br>조명이 켜지고 감독과 출연진이 무대에 오르면서 박수 소리가 더 커졌다. 전날 오후 기자회견장 앞에서 김무열의 사인을 받기 위해 기다리던 티모 베바는 영화가 끝난 뒤 "넷플릭스 시리즈에서 반한 김무열을 보려고 뮌헨에서 올라왔다. 그런데 오늘 밤부터 마동석 팬도 되기로 했다"고 말했다.`,
+        "imgUrl":`https://img3.yna.co.kr/etc/inner/KR/2024/02/24/AKR20240224023800082_12_i_P4.jpg`,
+        "createdAt":`2024-02-24`,
+        "source": `https://www.yna.co.kr/view/AKR20240224023800082?section=entertainment/movies`
+      },
+      {
+        'title':`비틀스 멤버들 그린 영화 4편 만든다…샘 멘데스 감독 메가폰`,
+        'subtitle': `링고스타 "우리 모두 샘 멘데스 영화 프로젝트 지지"`,
+        "content":`전설적인 밴드 비틀스 멤버들을 주인공으로 한 영화 4편이 할리우드에서 제작된다고 미 연예매체 데드라인이 20일(현지시간) 전했다.<br>보도에 따르면 할리우드의 유명 감독인 샘 멘데스의 제안으로 이 영화 프로젝트가 추진돼 투자배급사 소니픽처스 등과 합의를 마쳤다.<br>멘데스 감독이 영화 4편을 모두 연출하고 영화 제작 전반에도 직접 참여한다.<br>이번 영화 프로젝트는 비틀스 멤버인 폴 매카트니와 링고스타, 고인이 된 존 레넌과 조지 해리슨의 유족이 모두 이들의 생애에 대한 영화화와 음악 사용을 허락한 첫 번째 사례라고 데드라인은 전했다.`,
+        "imgUrl":`https://img5.yna.co.kr/photo/ap/2024/01/12/PAP20240112261201009_P4.jpg`,
+        "createdAt":`2024-02-21`,
+        "source": `https://www.yna.co.kr/view/AKR20240221054100075?section=entertainment/movies`
+      },
+      {
+        'title':`정우성·이정재 20대 시절 다시 본다…'비트''태양은 없다' 재개봉`,
+        'subtitle': `김성수 감독의 '비트'와 '태양은 없다'는 다음 달 6일과 13일 CGV에서 각각 재개봉한다.`,
+        "content":`두 작품은 불안한 삶을 지나는 젊은이들의 사랑과 우정을 담은 1990년대 대표 청춘 영화다.<br>1997년 처음 극장에 걸린 '비트'는 타고난 싸움꾼이자 주변인 민(정우성 분)의 이야기를 그렸다. 정우성 외에도 고소영, 유오성, 임창정 등이 출연했다.`,
+        "imgUrl":`https://img9.yna.co.kr/etc/inner/KR/2024/02/16/AKR20240216053800005_01_i_P4.jpg`,
+        "createdAt":`2024-02-16`,
+        "source": `https://www.yna.co.kr/view/AKR20240216053800005?section=entertainment/movies`
+      },    
+];
+
+
+
+// 화면을 만들어 보자
+
+let movie1List=[], movie2List =[]
+let searchedList=[]
+let popData=[], latestData=[], randomData=[]
+let koMovieList=[], enMovieList =[]
+let newsList =[]
+
+let totalResults =0 
+let page =1
+let pageSize = 20 // 한페이지에 보여질 item갯수
+const API_KEY = "45dfb0ff4a22c1a95d0041615999de56";
+let url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+
+const movie1 = document.querySelector('.movie1')
+const movie2 = document.querySelector('.movie2')
+const news = document.querySelector('.news')
+const input = document.querySelector('input')
+
+async function getMovies(query){
+  if (query.keyword == null){
+    url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  } else if (query.keyword != null && query.keyword != 'detail'){
+    url =`https://api.themoviedb.org/3/search/movie?query=${query.keyword}&api_key=${API_KEY}`
+
+  } else if(query.keyword == 'detail'){
+    url = `https://api.themoviedb.org/3/movie/${query.movieId}?api_key=${API_KEY}`
+  } else{
+    return;
+  }
+
+  const movieUrl = new URL(url);
+  movieUrl.searchParams.set('page', page)
+  movieUrl.searchParams.set('pageSize', pageSize)
+  try{
+    const response = await fetch(movieUrl);
+    const data = await response.json()
+    if (response.status == 200){
+      movie1List = data.results; // result에 배열 있다.
+      console.log('movie1List : ', movie1List);
+      if(movie1List.length ==0){
+        throw new Error('No result for the search')
+      }
+    } else{
+      throw new Error('예상치 못한 에러가 발생했습니다.')
+    }
+  } catch(e){
+    console.log(e.message);
+    errorRender(e.message)
+  }
+}
+
+async function getGlobalMovies(nation){
+  let country = nation;
+  url = url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  const movieUrl = new URL(url);
+  movieUrl.searchParams.set('page', page)
+  movieUrl.searchParams.set('pageSize', pageSize)
+  let tempList =[];
+  try{
+    const response = await fetch(movieUrl);
+    const data = await response.json()
+    if (response.status == 200){
+      tempList = data.results; // result에 배열 있다.
+      if(tempList.length ==0){
+        throw new Error('No result for the search')
+      }
+    } else{
+      throw new Error('예상치 못한 에러가 발생했습니다.')
+    }
+  } catch(e){
+    console.log(e.message);
+    errorRender(e.message)
+  }
+
+  if(country =='ko'){   // original_language ='ko'
+    movie2List = tempList.filter(movie => movie.original_language == 'ko')
+  } else{
+    movie2List = tempList.filter(movie => movie.original_language != 'ko')
+  }
+}
+
+//! 실행 코드 
+init()
+
+
+
+
+function init(){
+  // 받은 데이터가 너무 많아서 우선 12개로만 한다.
+  getMovies() 
+  //!한국영화 데이터받기
+  getGlobalMovies('ko')  // 'en' 
+  //!뉴스데이터 받기
+  newsList = newsData 
+  
+  render()
+}
+
+
+
+
+function render(){   
+  console.log('map 이전 movie1List :', movie1List )
+   //movie1 화면그리기
+   movie1.innerHTML='';
+   if(movie1List.length==0){
+      errorRender('No result found for the keyword!')
+   } else{
+     let movie1HTML = movie1List.map( movie =>{ 
+          const image = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+          return `
+          <div class="card" style="width: 10rem;">
+              <img src="${image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <p class="card-text">${movie.title}</p>
+              </div>
+          </div>
+      `}).join('')
+  
+      movie1.innerHTML = movie1HTML;
+  
+      //movie2 화면그리기
+      movie2.innerHTML=``;
+      let movie2HTML = movie2List.map( movie =>{
+          const image = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+          return `
+          <div class="card" style="width: 10rem;">
+              <img src="${image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <p class="card-text">${movie.title}</p>
+              </div>
+          </div>
+      `}).join('')
+      movie2.innerHTML = movie2HTML;
+  
+      //영화뉴스 화면
+      news.innerHTML=``;
+      let newsHTML = newsList.map( news =>{
+          const image = `${news.imgUrl}`;
+          return `
+          <div class="card" style="width: 20rem;">
+              <img src="${image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <p class="card-text">${news.title}</p>
+                  <p class="card-text">${news.subtitle}</p>
+              </div>
+          </div>
+      `}).join('')
+      news.innerHTML = newsHTML;
+   }
+}
+
+function errorRender(message){
+    movie1.innerHTML ='';
+    const errorHTML = `
+        <div class="alert alert-danger" role="alert">
+            ${message}
+        </div>
+    `;
+    movie1.innerHTML= errorHTML;
+}
+
+
+
